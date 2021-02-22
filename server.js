9809f66b5c397fd9e450d7532344d8947019750e
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-02-22 21:06:35 +0100

    Add basic endpoints to root, home and about files

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get('/', (req, res) => {
+  res.send('<p>In the task it was not stated where should we point `/` so I just want point it at anything :)<p>');
+});
+
+app.get('/home', (req, res) => {
+  res.show('home.html');
+});
+
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
