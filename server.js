5aac54e2fa35345445c8e9a3609cdc6811903a4d
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-02-22 20:47:48 +0100

    Add path module and middleware for serving filenames' paths

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
